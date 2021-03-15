6737aeb2a7b2ede40faa87901f5dfa3f6400b775
kriskut08 <73740605+kriskut08@users.noreply.github.com>
2021-03-15 12:02:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+	var penColour = 'black';
+
+	function setPenColour(pen) {
+	    penColour = pen;
+		document.getElementById("kv").style.backgroundColor = penColour;
+	}
+
+	function setPixelColour(pixel) {
+	    pixel.style.backgroundColor = penColour;
+	}
+	
+
